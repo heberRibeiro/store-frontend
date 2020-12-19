8654165a21c56953ad813e436dbcf50e08121778
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-12-19 18:09:05 -0300

    Product detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,23 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([{ path: '', component: ProductListComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent, TopBarComponent, ProductListComponent],
