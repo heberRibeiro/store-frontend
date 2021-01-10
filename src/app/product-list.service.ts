edc9b4f7f0c8831626dada03c20e1541d231b4f4
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2021-01-10 10:35:39 -0300

    Server

--- src/app/product-list.service.ts
+++ src/app/product-list.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-
 import { Product } from './product';
 
 @Injectable({
@@ -9,12 +7,10 @@ import { Product } from './product';
 })
 export class ProductListService {
   private productListUrl = 'http://localhost:8080/produto';
-  // products: any[] = [];
 
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    // return this.http.get('/assets/products.json');
     return this.http.get<Product[]>(this.productListUrl, {
       observe: 'response',
     });
